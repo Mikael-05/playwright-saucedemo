4902ac43dd4b98d187341a279e8850ef66bb2545
Mikael <mikael.lombard@hotmail.fr>
2026-07-24 03:49:28 +0200

    Premier commit

---
+++ pages/loginpage.ts
@@ -0,0 +1,49 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly username: Locator;
+  readonly password: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.username = page.getByPlaceholder('Username');
+    this.password = page.getByPlaceholder('Password');
+    this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async goto() {
+    await this.page.goto('https://www.saucedemo.com/');
+  }
+
+  async login(user: string, pwd: string) {
+    await this.username.fill(user);
+    await this.password.fill(pwd);
+    await this.loginButton.click();
+  }
+
+  async loginAsStandardUser() {
+    await this.login('standard_user', 'secret_sauce');
+  }
+
+  async loginFailed() {
+    await this.login('standard_user', 'secret_sauce1');
+  }
+
+  async verifyLoginSucceeded() {
+    await expect(this.page).toHaveURL(/inventory/);
+  }
+
+  async verifyLoginFailed() {
+    await expect(this.errorMessage).toBeVisible();
+  }
+
+async getLoginErrorMessage(): Promise<string> {
+  await expect(this.errorMessage).toBeVisible();
+  return await this.errorMessage.innerText();
+}
+
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/loginpage';
+
+test('Connexion avec un utilisateur valide', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.loginAsStandardUser();
+  await loginPage.verifyLoginSucceeded();
+});
+
+test('Connexion avec un mdp invalide', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.loginFailed();
+  await loginPage.verifyLoginFailed();
+  const error = await loginPage.getLoginErrorMessage();
+  expect(error).toContain(
+    'Epic sadface: Username and password do not match any user in this service'
+  );
+});
