95d02e57b3733b6e94a5ab46b8c759db7ef606a9
Mikael <mikael.lombard@hotmail.fr>
2026-08-21 12:16:44 +0200

    Test inventory page functionality

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -1,12 +1,19 @@
 // pages/InventoryPage.ts
 
-import { Page, Locator } from '@playwright/test';
+import { expect, Page, Locator } from '@playwright/test';
+
 
 export class InventoryPage {
   readonly cart: Locator;
+  readonly articles: Locator;
+  readonly sortDropdown: Locator;
+  readonly productPrices: Locator;
 
   constructor(private page: Page) {
     this.cart = page.locator('[data-test="shopping-cart-link"]');
+    this.articles = page.locator('.inventory_item');
+    this.sortDropdown = page.locator('[data-test="product-sort-container"]');
+    this.productPrices = page.locator('.inventory_item_price');
   }
 
   product(name: string): Locator {
@@ -23,5 +30,29 @@ export class InventoryPage {
 
   async openCart() {
     await this.cart.click();
+  }
+
+  async getArticleCount(): Promise<number> {
+    return await this.articles.count();
+  }
+
+  async sortByPriceAscending(): Promise<void> {
+    await this.sortDropdown.selectOption('lohi');
+  }
+
+  async getDisplayedPrices(): Promise<number[]> {
+    const prices = await this.productPrices.allTextContents();
+
+    return prices.map(price =>
+      Number(price.replace('$', ''))
+    );
+  }
+
+  async verifyPricesAreAscending(): Promise<void> {
+    const prices = await this.getDisplayedPrices();
+
+    const sortedPrices = [...prices].sort((a, b) => a - b);
+
+    expect(prices).toEqual(sortedPrices);
   }
 }
---
+++ tests/inventoryTests.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { InventoryPage } from '../pages/InventoryPage';
+
+
+test('Vérifie le nombre de produit', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+
+  await loginPage.goto();
+  await loginPage.loginAsStandardUser();
+  await loginPage.verifyLoginSucceeded();
+  const articleCount = await inventoryPage.getArticleCount();
+  expect(articleCount).toBe(6);
+});
+
+
+test('Trier les produits par prix croissant', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+
+  // Login
+  await loginPage.goto();
+  await loginPage.loginAsStandardUser();
+  await loginPage.verifyLoginSucceeded();
+
+  // Sort products by ascending price
+  await inventoryPage.sortByPriceAscending();
+
+  // Assert order of prices is ascending
+  await inventoryPage.verifyPricesAreAscending();
+});
+
+//await this.InventoryPage.addProduct(product);
